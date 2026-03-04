d3ab6f472705a5c4b666086d7a68e49d61b01685
erenmelek190542-coder <erenmelek190542@gmail.com>
2026-03-04 12:38:53 +0300

    Add files via upload

---
+++ bilgisayar programlama/ödev2/ödev2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	
+	int num1,num2;
+	printf("Enter first integer\n");
+	scan(%"d",&num1);
+	printf("Enter second integer\n");
+	scanf("%d",&num2);
+	
+	int sum = num1 + num2;
+	printf("Sum is %d\n",sum);
+	
+	return 0;
+}
